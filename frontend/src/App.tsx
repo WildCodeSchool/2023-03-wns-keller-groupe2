76f8ce3bc6eec4b47ba6739aaf43aa95fb18e617
Fostrankill <romain.pourchaux@gmail.com>
2023-06-07 17:28:01 +0200

    Addfooter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <p>Hello World!</p>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import "./footer.scss";
+import { FaFacebookF } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer>
+      {/* <a href="#" className="footer_logo">
+        Romain
+      </a> */}
+      <ul className="permalinks">
+        <li>
+          <a href="#top">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+      </div>
+      <div className=".footer_copyright">
+        <small>&copy; Rent Bike All rights reserved.</small>
+      </div>
+    </footer>
+  );
+}
