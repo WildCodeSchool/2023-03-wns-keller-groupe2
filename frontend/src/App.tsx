d212a84c0ab44a68b8c91aaa320ef3944b4680ef
Sax59 <waquetv@gmail.com>
2023-06-08 15:37:34 +0200

    Delete test route on app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login/Login";
-import SideFilter from "./Components/SideFilter/SideFilter";
 import "./App.scss";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/test" element={<SideFilter/>}/>
       </Routes>
       <Footer />
     </>
