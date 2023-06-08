f28ea80609a8e4a75ef0f47c0ac7b8a1f0fa4566
Priscillia <selvapriscillia@yahoo.fr>
2023-06-08 09:34:21 +0200

    add icon for purchase and account + css

--- frontend/src/Components/navBar.tsx
+++ frontend/src/Components/navBar.tsx
@@ -1,49 +1,57 @@
 import { Link } from "react-router-dom";
-import "./navBar.scss";
 import logo from "../Assets/Logo.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { VscAccount } from "react-icons/vsc";
+import "./navBar.scss";
 
 export default function NavBar() {
   return (
-    <nav className="navBar">
-      <ul className="item">
-        <img src={logo} alt="Logo Rent Bike" className="logo" />
-        <li className="list">
-          <Link to="/" className="link">
+    <nav className="navbar">
+      <ul className="navbar-item">
+        <img src={logo} alt="Logo Rent Bike" className="navbar-logo" />
+        <li className="navbar-list">
+          <Link to="/" className="navbar-link">
             Accueil
           </Link>
         </li>
-        <li className="list">
-          <Link to="/veloElec" className="link">
-            Vélo électrique
+        <li className="navbar-list">
+          <Link to="/veloElec" className="navbar-link">
+            Vélos électriques
           </Link>
         </li>
-        <li className="list">
-          <Link to="/vtt" className="link">
+        <li className="navbar-list">
+          <Link to="/vtt" className="navbar-link">
             VTT
           </Link>
         </li>
-        <li className="list">
-          <Link to="/veloPro" className="link">
-            Vélo professionelles
+        <li className="navbar-list">
+          <Link to="/veloPro" className="navbar-link">
+            Vélos professionnels
           </Link>
         </li>
-        <li className="list">
-          <Link to="/veloVintage" className="link">
-            Vélo Vintage
+        <li className="navbar-list">
+          <Link to="/veloVintage" className="navbar-link">
+            Vélos Vintages
           </Link>
         </li>
-        <li className="list">
-          <Link to="/accessoires" className="link">
+        <li className="navbar-list">
+          <Link to="/accessoires" className="navbar-link">
             Accessoires vélos
           </Link>
         </li>
-        <li className="list">
-          <Link to="/panier" className="link">
+        <li className="navbar-list navbar-list-icon">
+          <div className="navbar-icon">
+            <AiOutlineShoppingCart />
+          </div>
+          <Link to="/panier" className="navbar-link">
             Mon panier
           </Link>
         </li>
-        <li className="list">
-          <Link to="/compte" className="link">
+        <li className="navbar-list navbar-list-icon">
+          <div className="navbar-icon">
+            <VscAccount />
+          </div>
+          <Link to="/compte" className="navbar-link">
             Mon compte
           </Link>
         </li>
