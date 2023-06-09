d669778ac13564b7ba4c57ceac0be0828dc5d2fe
Priscillia <selvapriscillia@yahoo.fr>
2023-06-09 14:11:51 +0200

    uninstall nodemon

--- frontend/src/Pages/Accessories/Accessories.tsx
+++ frontend/src/Pages/Accessories/Accessories.tsx
@@ -1,14 +1,19 @@
 import CardProduct from "../../Components/CardProduct/CardProduct";
 import NavBar from "../../Components/NavBar/NavBar";
+import SideFilter from "../../Components/sideFilter/SideFilter";
 import "./accessories.scss";
 
 export default function Accessories() {
   return (
     <div>
       <NavBar />
-      <h1 className="accessories-title">Accessoires Vélos</h1>
-      <div className="accessories-card">
-        <CardProduct />
+      <div className="bike-display">
+        <SideFilter />
+        <div className="bike-parent-title-card">
+          <h1 className="bike-title">Accessoires</h1>
+          <div className="bike-card"></div>
+          <CardProduct />
+        </div>
       </div>
     </div>
   );
