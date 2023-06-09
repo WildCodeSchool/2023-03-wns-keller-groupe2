da664c133e7afdcb71cc5166e99f83dc5cb08e63
Priscillia <selvapriscillia@yahoo.fr>
2023-06-09 09:52:52 +0200

    add others pages

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import Banner from "../../Components/Banner/Banner";
 import NavBar from "../../Components/NarBar/NavBar";
 
 export default function Home() {
   return (
     <div>
+      <Banner />
       <NavBar />
     </div>
   );
