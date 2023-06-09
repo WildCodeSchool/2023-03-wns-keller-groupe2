1305c828a7993496e468666929d1e988b220b0c4
deabreudavid <de_abreu.david@yahoo.fr>
2023-06-09 10:10:43 +0200

    end first part visual homePage Presentation

--- frontend/src/Components/ResumeProject/ResumeProject.tsx
+++ frontend/src/Components/ResumeProject/ResumeProject.tsx
@@ -11,43 +11,38 @@ export default function ResumeProject() {
         className="logoResumeProject"
       ></img>
       <div className="textResume1">
-        <h1 className="titleResume">Rent Bike</h1>
+        <h1 className="titleResume">Rent Bike Qui sommes nous ?</h1>
         <h2 className="descriptionResume">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-          tristique, mauris eget iaculis scelerisque, diam libero bibendum sem,
-          quis blandit felis mauris nec nisi. Vivamus a lorem sit amet turpis
-          sollicitudin sagittis ut sit amet diam. Sed lectus magna, ultricies et
-          semper at, hendrerit non massa. Vestibulum non maximus lectus, at
-          blandit justo. Vestibulum nulla leo, lobortis a sem ultrices, auctor
-          mollis nulla. Nam interdum quam ac tempor porta. Sed sit amet ornare
-          purus, id mattis lectus. Mauris sed suscipit nisl. Nullam eu mi
-          molestie, tincidunt lectus sed, placerat eros. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Etiam tristique, mauris eget iaculis
-          scelerisque, diam libero bibendum sem, quis blandit felis mauris nec
-          nisi. Vivamus a lorem sit amet turpis sollicitudin sagittis ut sit
-          amet diam. Sed lectus magna, ultricies et semper at, hendrerit non
-          massa. Vestibulum non maximus lectus, at blandit justo. Vestibulum
-          nulla leo, lobortis a sem ultrices, auctor mollis nulla. Nam interdum
-          quam ac tempor porta. Sed sit amet ornare purus, id mattis lectus.
-          Mauris sed suscipit nisl. Nullam eu mi molestie, tincidunt lectus sed,
-          placerat molestie, tincidunt lectus sed, placerat eros. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Etiam tristique, mauris eget
-          iaculis scelerisque, diam libero bibendum sem, quis blandit felis
-          mauris nec nisi. Vivamus a lorem sit amet turpis sollicitudin sagittis
-          ut sit amet diam. Sed lectus magna, ultricies et semper at, hendrerit
-          non massa. Vestibulum non maximus lectus, at blandit justo. Vestibulum
-          nulla leo, lobortis a sem ultrices, auctor mollis nulla. Nam interdum
-          quam ac tempor porta. Sed sit amet ornare purus, id mattis lectus.
-          Mauris sed suscipit nisl. Nullam eu mi molestie, tincidunt lectus sed,
-          placerat eros. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit..Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-          tristique, mauris eget iaculis scelerisque, diam libero bibendum sem,
-          quis blandit felis mauris nec nisi. Vivamus a lorem sit amet turpis
-          sollicitudin sagittis ut sit amet diam. Sed lectus magna, ultricies et
-          semper at, hendrerit non massa. Vestibulum non maximus lectus, at
-          blandit justo.
+          Rent Bike est une solution de location de vélos qui rend vos
+          déplacements urbains pratiques et écologiques. Que vous soyez un
+          habitant local ou un touriste en visite, notre service de location de
+          vélos vous permet d'explorer la ville à votre rythme. Imaginez-vous
+          pédalant le long des rues pittoresques, découvrant des quartiers
+          animés et des sites emblématiques en chemin.
+          <br />
+          Avec Rent Bike, vous avez la liberté de créer votre propre itinéraire
+          et de vivre une expérience authentique. Notre processus de location
+          est simple et rapide. Il vous suffit de télécharger notre application
+          conviviale, de choisir la station de location la plus proche et de
+          réserver votre vélo en quelques clics. Nous proposons une large gamme
+          de vélos adaptés à tous les besoins : des vélos de ville élégants aux
+          VTT robustes pour les aventuriers. Lorsque vous récupérez votre vélo à
+          la station, vous constaterez que nos vélos sont bien entretenus, sûrs
+          et équipés de tout ce dont vous avez besoin pour une balade agréable.
+          <br />
+          <br />
+          Vous recevrez également des conseils sur les meilleurs itinéraires et
+          les attractions locales à ne pas manquer. Que vous souhaitiez vous
+          rendre au travail, visiter des musées, vous promener le long de la
+          côte ou simplement profiter de l'air frais, Rent Bike est là pour vous
+          accompagner. Vous pouvez louer nos vélos pour une heure, une journée
+          ou même plus longtemps, selon vos besoins. De plus, notre engagement
+          envers l'environnement se reflète dans nos pratiques durables. Nous
+          encourageons le vélo comme moyen de transport respectueux de
+          l'environnement et nous nous efforçons de réduire notre empreinte
+          carbone. Alors, prêt à découvrir la ville d'une manière nouvelle et
+          passionnante ? Rejoignez Rent Bike dès aujourd'hui et embarquez pour
+          une aventure à deux roues inoubliable !
         </h2>
       </div>
     </div>
