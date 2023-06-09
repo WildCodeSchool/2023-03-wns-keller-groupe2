8e24ab52b6432fbb6d1cbc525f3f604f63d0b8b8
Priscillia <selvapriscillia@yahoo.fr>
2023-06-09 14:01:39 +0200

    layout pages and add swiper

--- frontend/src/Pages/VintageBike/VintageBike.tsx
+++ frontend/src/Pages/VintageBike/VintageBike.tsx
@@ -1,14 +1,19 @@
 import CardProduct from "../../Components/CardProduct/CardProduct";
 import NavBar from "../../Components/NavBar/NavBar";
+import SideFilter from "../../Components/sideFilter/SideFilter";
 import "./vintageBike.scss";
 
 export default function VintageBike() {
   return (
     <div>
       <NavBar />
-      <h1 className="vintage-bike-title">Vélos Vintages</h1>
-      <div className="vintage-bike-card">
-        <CardProduct />
+      <div className="bike-display">
+        <SideFilter />
+        <div className="bike-parent-title-card">
+          <h1 className="bike-title">Vélos Professionnels</h1>
+          <div className="bike-card"></div>
+          <CardProduct />
+        </div>
       </div>
     </div>
   );
