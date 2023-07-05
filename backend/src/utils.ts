ec6a4c60c036009ba2e19ba367fc4e1b13ddbe6a
Sax59 <waquetv@gmail.com>
2023-07-05 16:09:03 +0200

    Config utils with dotenv

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -3,13 +3,15 @@ import { Shop } from "./entity/Shop";
 import { Order } from "./entity/Order";
 import { Bike } from "./entity/Bike";
 import { Users } from "./entity/Users";
+import "dotenv/config";
 
 const dataSource = new DataSource({
   type: "postgres",
-  host: "",
+  host: process.env.HOST,
   port: 5432,
-  username: "",
-  password: "",
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   synchronize: true,
   entities: [Shop, Order, Bike, Users],
 });
