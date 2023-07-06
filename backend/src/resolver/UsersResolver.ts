33f4b2d2e4ce579ccce7eae91231e465e946d970
Priscillia <selvapriscillia@yahoo.fr>
2023-07-06 15:15:50 +0200

    add resolvers in index, fix resolver user, fix dockercompose, fix utils

--- backend/src/resolver/UsersResolver.ts
+++ backend/src/resolver/UsersResolver.ts
@@ -1,44 +1,191 @@
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt-ts";
-
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { GraphQLError } from "graphql";
+import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql";
 import { Users } from "../entity/Users";
 import dataSource from "../utils";
+import { JWT_SECRET } from "..";
 
 @Resolver()
 class UsersResolver {
   @Mutation(() => String)
   async createUser(
+    @Arg("firstName") firstname: string,
+    @Arg("lastName") lastname: string,
     @Arg("email") email: string,
-    @Arg("password") password: string
-  ): Promise<String> {
-    const user = new Users();
-    user.email = email;
-    user.password = await bcrypt.hash(password, 10);
-    await dataSource.getRepository(Users).save(user);
-    return "User created";
+    @Arg("password") password: string,
+    @Arg("gender") gender: string,
+    @Arg("dob") dob: Date,
+    @Arg("phonenum") phonenum: string,
+    @Arg("admin") admin: boolean
+  ): Promise<String | GraphQLError> {
+    try {
+      const user = new Users();
+      user.firstName = firstname;
+      user.lastName = lastname;
+      user.email = email;
+      user.password = await bcrypt.hash(password, 10);
+      user.gender = gender;
+      user.dob = dob;
+      user.phonenum = phonenum;
+      user.admin = admin;
+      await dataSource.getRepository(Users).save(user);
+      return "User created";
+    } catch (error) {
+      return new GraphQLError("An error occurred");
+    }
+  }
+
+  @Query(() => Users)
+  async getUserFromToken(
+    @Arg("token") token: string
+  ): Promise<Users | String | GraphQLError> {
+    try {
+      const decoded: any = jwt.verify(token, JWT_SECRET);
+      const user = await dataSource
+        .getRepository(Users)
+        .findOneByOrFail({ email: decoded.email });
+      return user;
+    } catch (err) {
+      return new GraphQLError("An error occured");
+    }
+  }
+
+  @Query(() => String)
+  async refreshToken(
+    @Arg("token") token: string
+  ): Promise<String | GraphQLError> {
+    try {
+      const decoded: any = jwt.verify(token, JWT_SECRET);
+      const user = await dataSource
+        .getRepository(Users)
+        .findOneByOrFail({ email: decoded.email });
+      delete decoded.iat;
+      delete decoded.exp;
+      delete decoded.nbf;
+      delete decoded.jti;
+      return jwt.sign({ email: user.email }, JWT_SECRET);
+    } catch (err) {
+      return new GraphQLError("An error occured");
+    }
+  }
+
+  @Query(() => String)
+  async verifyToken(
+    @Arg("token") token: string
+  ): Promise<String | GraphQLError> {
+    try {
+      const decoded: any = jwt.verify(token, JWT_SECRET);
+      const user = await dataSource
+        .getRepository(Users)
+        .findOneByOrFail({ email: decoded.email });
+      if (decoded != null && user != null) {
+        return "User is logged in";
+      } else {
+        return new GraphQLError("User is not logged in");
+      }
+    } catch (error) {
+      return new GraphQLError("An error occurred");
+    }
+  }
+
+  @Authorized()
+  @Query(() => [Users])
+  async getAllUsers(): Promise<Users[] | string | GraphQLError> {
+    try {
+      const users = await dataSource.getRepository(Users).find();
+      return users;
+    } catch (error) {
+      return new GraphQLError("An error occurred");
+    }
+  }
+
+  @Query(() => Users)
+  async getUser(
+    @Arg("userId") userId: number
+  ): Promise<Users | string | GraphQLError> {
+    try {
+      const user = await dataSource
+        .getRepository(Users)
+        .findOne({ where: { id: userId } });
+      if (user != null) {
+        return user;
+      } else {
+        return new GraphQLError("No user found");
+      }
+    } catch (error) {
+      return new GraphQLError("An error occured");
+    }
+  }
+
+  @Mutation(() => String)
+  async updateUser(
+    @Arg("userId") userId: number,
+    @Arg("firstName") firstname: string,
+    @Arg("lastName") lastname: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Arg("gender") gender: string,
+    @Arg("dob") dob: Date,
+    @Arg("phonenum") phonenum: string,
+    @Arg("admin") admin: boolean
+  ): Promise<String | GraphQLError> {
+    try {
+      const user = new Users();
+      user.firstName = firstname;
+      user.lastName = lastname;
+      user.email = email;
+      user.password = password;
+      user.gender = gender;
+      user.dob = dob;
+      user.phonenum = phonenum;
+      user.admin = admin;
+      await dataSource.getRepository(Users).update(userId, {
+        firstname,
+        lastname,
+        email,
+        password,
+        gender,
+        dob,
+        phonenum,
+        admin,
+      });
+      return `User ${userId} updated`;
+    } catch (error) {
+      return new GraphQLError("An error occurred");
+    }
+  }
+
+  @Mutation(() => String)
+  async deleteUser(
+    @Arg("userId") userId: number
+  ): Promise<String | GraphQLError> {
+    try {
+      await dataSource.getRepository(Users).delete(userId);
+      return "User deleted";
+    } catch (error) {
+      return new GraphQLError("An error occurred");
+    }
   }
 
   @Query(() => String)
   async login(
     @Arg("email") email: string,
     @Arg("password") password: string
-  ): Promise<String> {
+  ): Promise<String | GraphQLError> {
     const user = await dataSource
       .getRepository(Users)
       .findOneByOrFail({ email });
-
     try {
-      if (await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign({ email }, "supersecretkey");
+      if ((await bcrypt.compare(password, user.password)) as boolean) {
+        const token = jwt.sign({ email }, JWT_SECRET);
         return token;
       } else {
-        return "Wrong password";
+        return new GraphQLError("Wrong password");
       }
     } catch (err) {
-      return "error";
+      return new GraphQLError("An error occurred");
     }
   }
 }
-
 export default UsersResolver;
