7056179151460c7db1d0e3b2aee3c26a0d5b6ee0
Sax59 <waquetv@gmail.com>
2023-07-06 16:23:58 +0200

    Add delet and update query

--- backend/src/resolver/BikeResolver.ts
+++ backend/src/resolver/BikeResolver.ts
@@ -39,6 +39,49 @@ class BikeResolver {
       return new GraphQLError("An error accured");
     }
   }
+
+  @Mutation(() => String)
+  async updateBike(
+    @Arg("id") id: number,
+    @Arg("name") name: string,
+    @Arg("descritpion") description: string,
+    @Arg("disponibility") disponibility: boolean,
+    @Arg("size") size: number,
+    @Arg("gender") gender: string,
+    @Arg("dateMaintenance") dateMaintenance: Date
+  ): Promise<String | GraphQLError> {
+    try {
+      const bike = new Bike();
+      bike.name = name;
+      bike.description = description;
+      bike.disponibility = disponibility;
+      bike.size = size;
+      bike.gender = gender;
+      bike.dateMaintenance = dateMaintenance;
+      await dataSource.getRepository(Bike).update(id, {
+        name,
+        description,
+        disponibility,
+        size,
+        gender,
+        dateMaintenance,
+      });
+      return `Bike ${id} updated`;
+    } catch (error) {
+      return new GraphQLError("An error accured");
+    }
+  }
+
+  @Mutation(() => String)
+  async deleteBike(@Arg("id") id: number): Promise<String | GraphQLError> {
+    try {
+      await dataSource.getRepository(Bike).delete(id);
+      return "Bike deleted";
+    } catch (error) {
+      console.error(error);
+      return new GraphQLError("An error accured");
+    }
+  }
 }
 
 export default BikeResolver;
