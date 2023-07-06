fda9878cec7e0925396fd97c6d99e33fb5b96681
Sax59 <waquetv@gmail.com>
2023-07-06 15:21:29 +0200

    Add query one by id

--- backend/src/resolver/BikeResolver.ts
+++ backend/src/resolver/BikeResolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver, Arg } from "type-graphql";
 import { Bike } from "../entity/Bike";
 import dataSource from "../utils";
 
@@ -8,6 +8,11 @@ class BikeResolver {
   async getAllOrder(): Promise<Bike[]> {
     const result = await dataSource.getRepository(Bike).find();
     return result;
+  }
+
+  @Query(() => Bike)
+  async getBikeById(@Arg("id") id: string): Promise<Bike> {
+    return await dataSource.getRepository(Bike).findOneByOrFail(id);
   }
 }
 
