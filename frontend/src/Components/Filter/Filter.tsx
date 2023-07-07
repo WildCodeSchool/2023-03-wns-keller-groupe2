7f019f201b172384849de528f4a15cfdc242fa1b
Priscillia <selvapriscillia@yahoo.fr>
2023-07-07 16:35:15 +0200

    fetch api filter bike categories

--- frontend/src/Components/Filter/Filter.tsx
+++ frontend/src/Components/Filter/Filter.tsx
@@ -1,54 +1,44 @@
-import { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
 import RangeSlide from "../RangeSlide/RangeSlide";
 
-export default function Filter(){
-const [brands, setbrands] = useState([
-    {
-        id:1,
-        name:'Lapierre',
-    },
-    {
-        id:2,
-        name:'Rockrider',
-    },
-    {
-        id:3,
-        name:'Cannondale',
-    },
-    {
-        id:4,
-        name:'KTM',
-    },
-    {
-        id:5,
-        name:'Pinarello',
-    },
-    {
-        id:6,
-        name:'Look',
+interface Brand {
+  id: number;
+  name: string;
+}
+
+const GET_ALL_BRANDS = gql`
+  query Query {
+    getAllBikeCategories {
+      id
+      name
     }
-])
-    return(
-        <div className="filter">
-            <h2>Vos Filtres</h2>
-            <h3>Prix:</h3>
-            <RangeSlide />
-            <h3>Genre:</h3>
-            <ul>
-                <li>Homme</li>
-                <li>Femme</li>
-                <li>Enfant</li>
-            </ul>
-            <h3>Marques:</h3>
-            <ul>
-                {brands.map((brand)=>{
-                    return(
-                        <li key={brand.id}>{brand.name}</li>
-                    )
-                })}
-            </ul>
-            <h3>Durée:</h3>
-            <RangeSlide />
-        </div> 
-    );
+  }
+`;
+
+export default function Filter() {
+  const { loading, error, data } = useQuery(GET_ALL_BRANDS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div className="filter">
+      <h2>Vos Filtres</h2>
+      <h3>Prix:</h3>
+      <RangeSlide />
+      <h3>Genre:</h3>
+      <ul>
+        <li>Homme</li>
+        <li>Femme</li>
+        <li>Enfant</li>
+      </ul>
+      <h3>Marques:</h3>
+      <ul>
+        {data.getAllBikeCategories.map((brand: Brand) => {
+          return <li key={brand.id}>{brand.name}</li>;
+        })}
+      </ul>
+      <h3>Durée:</h3>
+      <RangeSlide />
+    </div>
+  );
 }
