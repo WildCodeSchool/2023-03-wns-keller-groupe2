2be5107c381923197dd0ccd44b076538e20ebd37
Sax59 <waquetv@gmail.com>
2023-07-07 14:59:02 +0200

    Fetch API for bike

--- frontend/src/Components/CardProduct/CardProduct.tsx
+++ frontend/src/Components/CardProduct/CardProduct.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Keyboard, FreeMode } from "swiper";
 import bike from "../../assets/bike.png";
@@ -16,29 +16,19 @@ interface Product {
   price: string;
 }
 
+const GET_ALL_BIKE = gql`
+  query GetAllShop {
+    getAllBike {
+      id
+      name
+    }
+  }
+`;
+
 export default function CardProduct() {
-  const [products, setProducts] = useState<Product[]>([
-    {
-      id: 1,
-      name: "Kalkhoff",
-      price: "1800 €",
-    },
-    {
-      id: 2,
-      name: "Moustache Bikes",
-      price: "1950 €",
-    },
-    {
-      id: 3,
-      name: "Trek",
-      price: "2100 €",
-    },
-    {
-      id: 4,
-      name: "Vélo de ville (VDV)",
-      price: "1750 €",
-    },
-  ]);
+  const { loading, error, data } = useQuery(GET_ALL_BIKE);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <div className="cardproduct-layout">
       <Swiper
@@ -47,8 +37,9 @@ export default function CardProduct() {
         pagination={{ clickable: true }}
         navigation
         modules={[FreeMode, Pagination, Keyboard]}
-        className="cardproduct-swiper">
-        {products.map((product) => {
+        className="cardproduct-swiper"
+      >
+        {data.getAllBike.map((product: Product) => {
           return (
             <div className="cardproduct-marge-swiper-">
               <SwiperSlide key={product.id} className="cardproduct-contour">
