4dd8ba1cafec0aa447323a5c7965ccbdd9c3a6d7
Priscillia <selvapriscillia@yahoo.fr>
2023-10-06 12:05:55 +0200

    fix side bar and heigh of card

--- frontend/src/Components/Filter/Filter.tsx
+++ frontend/src/Components/Filter/Filter.tsx
@@ -1,25 +1,6 @@
-import { useQuery, gql } from "@apollo/client";
 import RangeSlide from "../RangeSlide/RangeSlide";
 
-interface Brand {
-  id: number;
-  name: string;
-}
-
-const GET_ALL_BRANDS = gql`
-  query Query {
-    getAllBikeCategories {
-      id
-      name
-    }
-  }
-`;
-
 export default function Filter() {
-  const { loading, error, data } = useQuery(GET_ALL_BRANDS);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
   return (
     <div className="filter">
       <h2>Vos Filtres</h2>
@@ -30,12 +11,6 @@ export default function Filter() {
         <li>Homme</li>
         <li>Femme</li>
         <li>Enfant</li>
-      </ul>
-      <h3>Marques:</h3>
-      <ul>
-        {data.getAllBikeCategories.map((brand: Brand) => {
-          return <li key={brand.id}>{brand.name}</li>;
-        })}
       </ul>
       <h3>Durée:</h3>
       <RangeSlide />
