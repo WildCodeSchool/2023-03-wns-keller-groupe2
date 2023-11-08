97bb98528d9866aa0f00fe655ef2637e09dfa491
Priscillia <selvapriscillia@yahoo.fr>
2023-11-08 16:17:42 +0100

    test/bikeResolver-getBikeById

--- backend/__test__/resolver/BikeResolver.test.ts
+++ backend/__test__/resolver/BikeResolver.test.ts
@@ -4,6 +4,7 @@ import dataSource from "../../src/utils";
 
 jest.mock("../../src/utils");
 
+//test query getAllBike
 describe("BikeResolver", () => {
   let bikeResolver: BikeResolver;
 
@@ -48,5 +49,46 @@ describe("BikeResolver", () => {
     const result = await bikeResolver.getAllBike();
 
     expect(result).toEqual(mockBikes);
+  });
+});
+
+//test query getBikeById
+describe("getBikeById", () => {
+  let bikeResolver: BikeResolver;
+
+  beforeEach(() => {
+    bikeResolver = new BikeResolver();
+  });
+
+  it("should get a bike by id", async () => {
+    const mockBike: Bike = {
+      id: 3,
+      name: "Bike 3",
+      description: "Bike 3 description",
+      disponibility: true,
+      size: 1,
+      gender: "masculin",
+      dateMaintenance: new Date(),
+      price: 30,
+      shop_id: [],
+      imageId: [],
+      bikeCategoriesId: [],
+    };
+
+    (dataSource.getRepository as jest.Mock).mockReturnValue({
+      findOne: jest.fn().mockResolvedValue(mockBike),
+    });
+
+    const result = await bikeResolver.getBikeById(1);
+
+    expect(result).toEqual(mockBike);
+  });
+
+  it("should throw an error if bike not found", async () => {
+    (dataSource.getRepository as jest.Mock).mockReturnValue({
+      findOne: jest.fn().mockResolvedValue(null),
+    });
+
+    await expect(bikeResolver.getBikeById(1)).rejects.toThrow("Bike not found");
   });
 });
