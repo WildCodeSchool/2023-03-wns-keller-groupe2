c201f6486957f31536006348f96a6500e413e463
Sax59 <waquetv@gmail.com>
2023-11-08 17:12:29 +0100

    Add condtionnal render if user is already connected

--- frontend/src/Pages/CartStepTwo/CartStepTwo.tsx
+++ frontend/src/Pages/CartStepTwo/CartStepTwo.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 import LoginForm from "../../Components/LoginForm/LoginForm";
+import { useContext } from "react";
+import { UserContext } from "../../services/context/userContext";
+import AlreadyConnect from "../../Components/AlreadyConnect/AlreadyConnect";
 import "./cartStepTwo.scss";
 
 export default function CartStepTwo() {
+  const users = useContext(UserContext);
   return (
-    <section className="connexion">
-      <header>
-        <h2>Connexion</h2>
-        <p>Veuillez vous connecter a votre compte</p>
-      </header>
-      <LoginForm />
-      <Link to="/login">Se créer un compte</Link>
-    </section>
+    <>
+      {users?.user.id ? (
+        <AlreadyConnect />
+      ) : (
+        <section className="connexion">
+          <header>
+            <h2>Connexion</h2>
+            <p>Veuillez vous connecter a votre compte</p>
+          </header>
+          <LoginForm />
+          <Link to="/login">Se créer un compte</Link>
+        </section>
+      )}
+    </>
   );
 }
