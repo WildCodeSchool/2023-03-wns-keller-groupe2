c0983180dd7d803e0df5ad7fcafad80f659eb25b
Priscillia <selvapriscillia@yahoo.fr>
2023-11-10 16:28:05 +0100

    fix/filterPages: filter for category for fetch in page

--- frontend/src/Components/CardProduct/CardProduct.tsx
+++ frontend/src/Components/CardProduct/CardProduct.tsx
@@ -58,31 +58,28 @@ export default function CardProduct({ category, onError }: CardProductProps) {
   return (
     <div className="cardproduct-container">
       <div className="cardproduct-grid">
-        {data.getAllBike.map((product: Product) => {
-          if (product.bikeCategoriesId.name === category)
-            return (
-              <div className="cardproduct" key={product.id}>
-                <img
-                  src={product.imageId[0].url}
-                  alt={product.name}
-                  className="cardproduct-image "
-                />
-                <div className="cardproduct-layout-button">
-                  <div>
-                    <p className="cardproduct-name">{product.name}</p>
-                    <p>{product.price}</p>
-                  </div>
-                  <div>
-                    <div className="cardproduct-margin-button">
-                      <ButtonMoreInfo id={product.id} />
-                    </div>
-                    <div>
-                      <ButtonRent id={product.id} />
-                    </div>
-                  </div>
+        {filteredProducts.map((product: Product) => {
+          <div className="cardproduct" key={product.id}>
+            <img
+              src={product.imageId[0].url}
+              alt={product.name}
+              className="cardproduct-image "
+            />
+            <div className="cardproduct-layout-button">
+              <div>
+                <p className="cardproduct-name">{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+              <div>
+                <div className="cardproduct-margin-button">
+                  <ButtonMoreInfo id={product.id} />
+                </div>
+                <div>
+                  <ButtonRent id={product.id} />
                 </div>
               </div>
-            );
+            </div>
+          </div>;
         })}
       </div>
     </div>
