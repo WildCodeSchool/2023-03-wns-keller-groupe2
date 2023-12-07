5ad06a33af0e8466c6b10d1600c52d30aab487c7
Priscillia <selvapriscillia@yahoo.fr>
2023-12-07 12:24:43 +0100

    add security on projects: crsf, rate limiting, manage errors jwt, authchecker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import "reflect-metadata";
 import * as jwt from "jsonwebtoken";
+import express from "express";
+import cookieParser from "cookie-parser";
+import csrf from "csurf";
+import rateLimit from "express-rate-limit";
 import { buildSchema } from "type-graphql";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import dataSource from "./utils";
 import BikeResolver from "./resolver/BikeResolver";
 import OrderResolver from "./resolver/OrderResolver";
@@ -24,7 +28,6 @@ const start = async (): Promise<void> => {
       BikeCategoriesResolver,
     ],
     authChecker: ({ context }) => {
-      console.log("context from authChecker", context);
       if (context.email !== undefined) {
         return true;
       } else {
@@ -40,19 +43,55 @@ const start = async (): Promise<void> => {
         req.headers.authorization !== undefined &&
         req.headers.authorization !== ""
       ) {
-        const payload = jwt.verify(
-          req.headers.authorization.split("Bearer ")[1],
-          "supersecretkey"
-        );
-        return payload;
+        try {
+          const payload = jwt.verify(
+            req.headers.authorization.split("Bearer ")[1],
+            process.env.JWT_SECRET as string
+          );
+          return { user: payload };
+        } catch (err) {
+          console.error("JWT verification failed:", err);
+          throw new Error("Authentication failed");
+        }
       }
       return {};
     },
   });
 
-  const { url } = await server.listen();
-  console.log(`🚀  Server ready at ${url}`);
-  console.log("hello hot reload ?");
-};
+  // create express app
+  const app = express();
 
-void start();
+  // we need this because "cookie" is true in csrfProtection
+  app.use(cookieParser());
+
+  // setup route middlewares
+  const csrfProtection = csrf({ cookie: true });
+  const parseForm = express.urlencoded({ extended: false });
+
+  app.get("/form", csrfProtection, function (req, res) {
+    // pass the csrfToken to the view
+    res.render("send", { csrfToken: req.csrfToken() });
+  });
+
+  app.post("/process", parseForm, csrfProtection, function (req, res) {
+    res.send("data is being processed");
+  });
+
+  // Apply rate limit middleware
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  });
+  app.use(limiter);
+
+  server.applyMiddleware({ app });
+
+  const port = process.env.PORT_BACKEND;
+  app.listen(port, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    );
+  });
+
+  void start();
+};
