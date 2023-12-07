00126d987a768fa4c037c9688068d65a0a57b382
sax59 <waquetv@gmail.com>
2023-12-07 17:50:22 +0100

    Feature: add card form

--- frontend/src/Components/PaymentForm/PaymentForm.tsx
+++ frontend/src/Components/PaymentForm/PaymentForm.tsx
@@ -2,13 +2,28 @@ import "./paymentForm.scss";
 
 export default function PaymentForm() {
   return (
-    <form>
-      <label htmlFor="">
-        <input type="text" />
+    <form className="style-form">
+      <h2>Coordonées de la carte</h2>
+      <label htmlFor="cardNum" className="payment-label">
+        Numéro de la carte
+        <input name="cardNum" type="text" className="form-input" />
       </label>
-      <label htmlFor="">
-        <input type="text" />
+      <label htmlFor="cardExpiry" className="payment-label">
+        Date d'expiration
+        <input name="cardExpiry" type="text" className="form-input" />
       </label>
+      <label htmlFor="CardCVV" className="payment-label">
+        CVV
+        <input name="cardCVV" type="text" className="form-input" />
+      </label>
+      <h2>Tituliare de la carte</h2>
+      <label htmlFor="cardName" className="payment-label">
+        Titulaire de la carte
+        <input name="cardName" type="text" className="form-input" />
+      </label>
+      <button type="submit" className="form-button">
+        Payer
+      </button>
     </form>
   );
 }
